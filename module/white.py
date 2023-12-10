83e9cd1d47fe57b1e2c4b3da64e8ce22bdb186a8
Ghislain Bernard <ghislain.bernard@gmail.com>
2023-12-10 23:37:49 +0100

    Insert 'module/white.py'

---
+++ module/white.py
@@ -0,0 +1,23 @@
+#--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
+
+import emoji
+
+import streamlit as st
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+def run(verbose: bool):
+
+  st.markdown('---')
+
+  number: int = st.session_state['number']
+  if verbose:
+    st.info('number = {}'.format(number), icon=emoji.emojize(':speech_balloon:'))
+
+  message: str = emoji.emojize(':white_large_square:') * number
+  if verbose:
+    st.info('message = "{}"'.format(message), icon=emoji.emojize(':speech_balloon:'))
+
+  st.markdown('# {}'.format(message))
+
+#--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
